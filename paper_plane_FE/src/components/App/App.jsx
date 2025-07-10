ee80bb69b68e9e9f3da6ad1394083997f1062287
Damian Lavin <damianlavin75@gmail.com>
2025-07-09 20:32:01 -0700

    Project setup complete.

---
+++ paper_plane_FE/src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import paperPlane from "../../assets/blue_pp_icon.png";
+
+import "./App.css";
+
+import Header from "../Header/Header";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="page">
+      <div className="page_content"></div>
+    </div>
+  );
+}
+
+export default App;
